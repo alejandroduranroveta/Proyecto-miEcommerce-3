0a4b16c313b8f271df4e4665c20ea10957d5c490
aleduranr <alejandroduranroveta@gmail.com>
2022-09-12 12:35:42 -0300

    commit para marge

--- api/routes/productsRoutes.js
+++ api/routes/productsRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { list} = require('../controllers/productsController');
+const {list} = require('../controllers/productsController');
 const router = express.Router();
 
 router.get('/products', list);
--- server.js
+++ server.js
@@ -2,17 +2,31 @@ const express = require('express');
 const productsRoutes = require('./routes/productsRoutes')
 const app = express();
 
+
+
+
+
+
+
+
+
+
 //Esto es para parsear el body que viene en formato JSON
-app.use(express.json());
+//app.use(express.json());
 
 //Aquí debajo van las Rutas a los endpoints
 //Por ejemplo: '/api'
-app.get('/products', (req, res) => {
+app.get('/', (req, res) => {
    res.send('<h1>Inicio</h1>');
 });
-app.use('/products',moviesRoutes)
+
+app.use("/api/v1/products", productRoutes);
 
 //Levantar servidor desde express
 app.listen(3000, () => {
    console.log('Servidor corriendo en puerto 3000');
 });
+
+
+
+
