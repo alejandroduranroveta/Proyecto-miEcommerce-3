0098e64b6642c206f6c7a8dd50a114f67dac3db2
Román Ferreira <romanferreira24@gmail.com>
2022-09-12 16:42:28 -0300

    Actualizando datos server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const app = expres.app();
+const app = express();
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
+app.use(express.json());
+require('dotenv').config(); 
 
+// app.use("/api/v1/products",productRoutes);
+// app.use("/api/v1/users",userRoutes);
 
-app.use("/api/v1/products",productRoutes);
-app.use("/api/v1/users",userRoutes);
+app.listen(3000,()=>console.log("Se levanto el server"));
