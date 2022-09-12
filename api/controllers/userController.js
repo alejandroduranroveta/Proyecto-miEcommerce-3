e2170423c321c6a4e487f78b2044d8a403832ca6
Román Ferreira <romanferreira24@gmail.com>
2022-09-12 16:03:35 -0300

    UserCreate

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -20,7 +20,39 @@ const userController = {
             user!==undefine?res.status(200).json(user):res.status(404).json({msg:"Not fund user"});
         } catch (error) {
             console.log(error);
-            res.status(404).json({mg:"Not fund"});
+            res.status(500).json({msg:"Error server"});
+        }
+    },
+    createUser:(req,res)=>{
+        try {
+            let {id,email="exaple@gmail.com",username="pepito",password="123",firtsname="Anonymous",lastname="Anonymous",profilepic="www.img.com"} = req.body;
+            let newUser = {
+                id,
+                email,
+                username,
+                password,
+                firtsname,
+                lastname,
+                profilepic
+            };
+            const bdUser = fs.readFileSync(path.join(__dirname,"/../","users.json"),"utf-8");
+            let users = JSON.parse(bdUser);
+            users.push(newUser);
+            fs.writeFileSync(path.join(__dirname,"/../","users.json"),JSON.stringify(users));
+            req.method!=="POST"?res.status(201).json(newUser):res.status(400).json({msg:"You need use POST method for create user"});
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({msg:"Error server"});
+        }
+        
+    },
+    modifyUser:(req,res)=>{
+        try {
+            
+        } catch (error) {
+            
         }
     }
 }
+
+module.exports = userController;
