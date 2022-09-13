f1d52f2b63e6c857820f28be23957e4eee21f07a
Román Ferreira <romanferreira24@gmail.com>
2022-09-13 16:34:49 -0300

    Arreglando pequeñas cosas

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router =express.Router();
 const userController = require("../controllers/userController")
+const isGod = require("../middlewares/isGod");
 
 router.get("/",userController.list);
 router.get("/:id",userController.searchById);
-router.post("/",userController.createUser);
+router.post("/",isGod,userController.createUser);
 router.put("/:id",userController.modifyUser);
 router.delete("/:id",userController.delete);
 
