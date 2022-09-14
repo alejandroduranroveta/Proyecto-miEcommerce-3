23ee2159ac7865cb26e7654a379a6c4ff32e4884
Federico de Fortuny <fdefortuny@gmail.com>
2022-09-14 16:17:55 -0300

    se crea login de usuarios

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router =express.Router();
 const userController = require("../controllers/userController")
 const isGod = require("../middlewares/isGod");
-const isGuest = require("../middlewares/isGuest");
 const isAdmin = require("../middlewares/isAdmin");
 
+
+
+router.post("/login",userController.login);
 router.get("/",isAdmin,userController.list);
 router.get("/:id",isAdmin,userController.searchById);
 router.post("/",isGod,userController.createUser);
